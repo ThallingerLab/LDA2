b9dc6a28a3b1e5e3c60aa8249a59170295389631
hartler <hartler@20-PX008.pers.ad.uni-graz.at>
2021-08-23 11:16:55 +0200

    test access

--- src/at/tugraz/genome/lda/xml/AddScan.java
+++ src/at/tugraz/genome/lda/xml/AddScan.java
@@ -40,7 +40,7 @@ public interface AddScan extends CgIAddScan
   public float getLowerThreshold();
 
   /**
-   * @return the uppererer m/z threshold that was used for reading
+   * @return the upperer m/z threshold that was used for reading
    */
   public float getUpperThreshold();
   
