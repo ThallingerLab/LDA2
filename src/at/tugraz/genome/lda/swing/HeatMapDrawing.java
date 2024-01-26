48955b709d43556aac2f4aa0670e6b5293e1b434
Leonida M. Lamp <leonidalamp@gmail.com>
2024-01-26 11:43:49 +0100

    Bug fix: selecting and deselecting single spots in the heatmap

--- src/at/tugraz/genome/lda/swing/HeatMapDrawing.java
+++ src/at/tugraz/genome/lda/swing/HeatMapDrawing.java
@@ -956,7 +956,7 @@ public class HeatMapDrawing extends JPanel implements ActionListener
         if (actionCommand.equalsIgnoreCase("Quant. anal. at not found"))
           confirmationMessage += "The peak nearest to the RT of the following experiments are taken:";
         else if (actionCommand.equalsIgnoreCase("Take exact peak for others"))
-          confirmationMessage += "The exact peak borders of the following experiments are taekn:";
+          confirmationMessage += "The exact peak borders of the following experiments are taken:";
         confirmationMessages.add(confirmationMessage);
         for (SimpleValueObject species : templateSpecies)
           confirmationMessages.add(species.getLabel()+":    "+species.getValue());
@@ -981,12 +981,12 @@ public class HeatMapDrawing extends JPanel implements ActionListener
       Graphics2D g2 = (Graphics2D)renderedImage_.getGraphics();
       boolean printAttentionRectangle = false;
       if (actionCommand.equalsIgnoreCase("Select")){
-        if (!this.selectAnalyte(moleculeNames_.get(lastClickedCellPos_[1]),experimentNames_.get(lastClickedCellPos_[0]),heatmap_.getColorForCell(lastClickedCellPos_[1], lastClickedCellPos_[0]),modHash,
+        if (!this.selectAnalyte(heatmap_.getOriginalAnalyteName(lastClickedCellPos_[1]),experimentNames_.get(lastClickedCellPos_[0]),heatmap_.getColorForCell(lastClickedCellPos_[1], lastClickedCellPos_[0]),modHash,
             vo.getAbsoluteFilePath(),updateableAndAnalyteBefore,maxIsotope))
           return;
         g2.setColor(Color.BLUE);
       }else if (actionCommand.equalsIgnoreCase("Deselect")){
-        g2.setColor(this.deselectAnalyte(moleculeNames_.get(lastClickedCellPos_[1]),experimentNames_.get(lastClickedCellPos_[0])));
+        g2.setColor(this.deselectAnalyte(heatmap_.getOriginalAnalyteName(lastClickedCellPos_[1]),experimentNames_.get(lastClickedCellPos_[0])));
         if (isMarkDoublePeaks() && heatmap_.getAttentionProbe(lastClickedCellPos_[1], experimentNames_.get(lastClickedCellPos_[0])) != null)
         {
           printAttentionRectangle = true;
@@ -996,7 +996,7 @@ public class HeatMapDrawing extends JPanel implements ActionListener
       cellRect.setLocation(new Point(cellRect.getLocation().x+imagePositionX_,cellRect.getLocation().y+imagePositionY_));
       g2.fillRect(cellRect.x+1,cellRect.y+1,cellRect.width-1,cellRect.height-1);
       if (printAttentionRectangle){
-        heatmap_.paintAttentionRectangle(g2, heatmap_.getAttentionProbe(moleculeNames_.get(lastClickedCellPos_[1]), experimentNames_.get(lastClickedCellPos_[0])), 
+        heatmap_.paintAttentionRectangle(g2, heatmap_.getAttentionProbe(lastClickedCellPos_[1], experimentNames_.get(lastClickedCellPos_[0])), 
             lastClickedCellPos_[1], lastClickedCellPos_[0], heatmap_.getExpressionImageXStart(), heatmap_.getExpressionImageYStart());
       }
       this.invalidate();
@@ -1218,7 +1218,7 @@ public class HeatMapDrawing extends JPanel implements ActionListener
                   	if (attentionProbe != null && attentionProbe == LipidomicsHeatMap.ATTENTION_COLOR_DOUBLE_PEAK || attentionProbe == LipidomicsHeatMap.ATTENTION_DOUBLE_AND_NOT_ALL_MODS)
                       this.mouseMoved(e);
                     else{
-                      if (isAnalyteSelected(heatmap_.getAnalyteName(cellPos[1]),experimentNames_.get(cellPos[0]))){
+                      if (isAnalyteSelected(heatmap_.getOriginalAnalyteName(cellPos[1]),experimentNames_.get(cellPos[0]))){
                         selectSingleItem_.setEnabled(false);
                         deselectSingleItem_.setEnabled(true);
                       }else{
